e0458001ff5a86ae64c12244b6775d6bb124bbc4
VadAxel <58472614+VadAxel@users.noreply.github.com>
2020-05-04 16:04:13 +0200

    uppdate

--- js/hemsida.js
+++ js/hemsida.js
@@ -20,25 +20,3 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
-//Knapp för att boka//
-
-function passWord() {
-  var testV = 1;
-  var pass1 = prompt('Skriv in det lösenord du fått på sms.', ' ');
-  while (testV < 3) {
-    if (!pass1)
-      history.go(-1);
-    if (pass1.toLowerCase() == "1234") {
-      alert('Rätt lösenord!');
-      window.open('2index.html');
-      break;
-    }
-    testV += 1;
-    var pass1 =
-      prompt('Åtkommst nekad - Lösenordet fel, försök igen.', 'Password');
-  }
-  if (pass1.toLowerCase() != "Lösenord" & testV == 3)
-    history.go(-1);
-  return " ";
-}
